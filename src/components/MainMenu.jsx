7202b6f79f75b239b8f13960c7d4951b73259f8a
martinasartori97 <martinasartori9@gmail.com>
2024-11-25 17:34:40 +0100

    edit js

--- src/components/MainMenu.jsx
+++ src/components/MainMenu.jsx
@@ -3,9 +3,8 @@ export default function MainMenu() {
     return (
         <nav>
             <NavLink to="/">Home</NavLink>
-            <NavLink to="/ChiSiamo">"chi siamo</NavLink>
-            <NavLink to="/Posts">“Posts” </NavLink>
-
+            <NavLink to="/ChiSiamo">chi siamo</NavLink>
+            <NavLink to="/Posts">Posts</NavLink>
         </nav>
     )
 }
