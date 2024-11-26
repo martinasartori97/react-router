0e00460630d85db5070be57e94bb86433153c72f
martinasartori97 <martinasartori9@gmail.com>
2024-11-26 15:02:33 +0100

    edit js

--- src/components/MainMenu.jsx
+++ src/components/MainMenu.jsx
@@ -1,7 +1,7 @@
 import { NavLink } from "react-router-dom";
 export default function MainMenu() {
     return (
-        <nav>
+        <nav className="navigazione">
             <NavLink to="/">Home</NavLink>
             <NavLink to="/ChiSiamo">chi siamo</NavLink>
             <NavLink to="/Posts">Posts</NavLink>
